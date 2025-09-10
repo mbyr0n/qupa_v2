214d03ac87f2f76584db1709e6f8df41de2e84d9
jmsantost <jmsantos@espol.edu.ec>
2025-09-10 04:15:34 -0500

    LEDS

--- foot-bot/simulator/qtopengl_footbot.h
+++ foot-bot/simulator/qtopengl_footbot.h
@@ -6,9 +6,11 @@ namespace argos {
 }
 
 #ifdef __APPLE__
-#include <gl.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
 #else
 #include <GL/gl.h>
+#include <GL/glu.h>
 #endif
 
 namespace argos {
@@ -16,16 +18,33 @@ namespace argos {
    class CQTOpenGLFootBot {
 
    public:
+
       CQTOpenGLFootBot();
+
       virtual ~CQTOpenGLFootBot();
+
       virtual void Draw(CFootBotEntity& c_entity);
 
    private:
+
+      /**
+       * @brief Renderiza el cuerpo del robot en una display list.
+       */
       void Render();
 
+      /**
+       * @brief Dibuja los LEDs de la entidad del foot-bot.
+       */
+      void DrawLEDs(CFootBotEntity& c_entity);
+
    private:
+
       /* Display list para el cuerpo del robot */
       GLuint m_unBodyList;
+      
+      /* Display list para un único LED (una esfera pequeña) */
+      GLuint m_unLEDList;
+
    };
 }
 
